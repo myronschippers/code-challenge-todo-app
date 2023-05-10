a939e09459ddd1c28c9d7871f6605847b608e50a
Myron R Schippers Jr <dubtwee@gmail.com>
2023-05-10 15:29:49 -0400

    adjusting the file structure for generated component as template files

--- tools/templates/component-template/__name__.tsx
+++ tools/templates/component-template/__name__.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import { Styled__name__Container } from './styles';
+import { I__name__Props } from './types';
 
-interface IProps {}
-
-export const __name__: React.FC<IProps> = (props) => {
-  return <div>__name__(sentenceCase)</div>;
+export const __name__: React.FC<I__name__Props> = ({ children }) => {
+  return <Styled__name__Container>__name__(sentenceCase)</Styled__name__Container>;
 };
---
+++ tools/templates/component-template/styles.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Styled__name__Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
