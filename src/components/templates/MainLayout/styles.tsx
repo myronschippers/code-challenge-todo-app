81996816c19f83d86de60ed406cd5a9c2e85defb
Myron R Schippers Jr <dubtwee@gmail.com>
2023-05-15 20:38:37 -0400

    adding styling for the background image

--- src/components/templates/MainLayout/styles.tsx
+++ src/components/templates/MainLayout/styles.tsx
@@ -1,9 +1,19 @@
 import styled from 'styled-components';
 
 export const StyledMainLayoutContainer = styled.div`
+  &::before {
+    content: '';
+    width: 100%;
+    height: 300px;
+    background: url('/images/bg-desktop-dark.jpg');
+    background-size: cover;
+    position: absolute;
+    z-index: -1;
+  }
+`;
+
+export const StyledMainLayoutMainContent = styled.main`
   display: flex;
   flex-direction: column;
   width: 100%;
 `;
-
-export const StyledMainLayoutMainContent = styled.main``;
