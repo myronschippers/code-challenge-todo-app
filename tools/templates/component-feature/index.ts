eaf71c2c919f2ab77129a52ec14d8bda16a7fe26
Myron R Schippers Jr <dubtwee@gmail.com>
2023-05-16 20:35:30 -0400

    updating feature component generated template to match the othe component file structures

--- tools/templates/component-feature/index.ts
+++ tools/templates/component-feature/index.ts
@@ -1,1 +1,2 @@
 export { __name__ } from './__name__';
+export type { I__name__Props } from './types';
---
+++ tools/templates/component-feature/types.ts
@@ -0,0 +1,5 @@
+import { ReactNode } from 'react';
+
+export interface I__name__Props {
+  children?: ReactNode;
+}
