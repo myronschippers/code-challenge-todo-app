a92f6fae24eba8abb1b7491e938bfc4228e0f901
Myron R Schippers Jr <dubtwee@gmail.com>
2023-05-16 23:37:27 -0400

    correcting styled-components loading issue with nextjs 13 app setup

--- src/components/templates/MainLayout/MainLayout.tsx
+++ src/components/templates/MainLayout/MainLayout.tsx
@@ -1,20 +1,26 @@
 'use client';
 
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 import { MainHeader } from '@/components/templates/MainHeader';
 import { Wrapper } from '@/components/templates/Wrapper';
 import { DesignOverlay } from '@/components/ui/DesignOverlay';
+import { GlobalStyles } from '@/theme/GlobalStyles';
+import * as theme from '@/theme';
 import { StyledMainLayoutContainer, StyledMainLayoutMainContent } from './styles';
 import { IMainLayoutProps } from './types';
 
 export const MainLayout: React.FC<IMainLayoutProps> = ({ children }) => {
   return (
-    <StyledMainLayoutContainer data-test-id="MainLayout">
-      <DesignOverlay />
-      <MainHeader />
-      <StyledMainLayoutMainContent>
-        <Wrapper>{children}</Wrapper>
-      </StyledMainLayoutMainContent>
-    </StyledMainLayoutContainer>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <StyledMainLayoutContainer data-test-id="MainLayout">
+        <DesignOverlay />
+        <MainHeader />
+        <StyledMainLayoutMainContent>
+          <Wrapper>{children}</Wrapper>
+        </StyledMainLayoutMainContent>
+      </StyledMainLayoutContainer>
+    </ThemeProvider>
   );
 };
--- src/libs/registry.tsx
+++ src/libs/registry.tsx
@@ -1,15 +1,22 @@
 'use client';
 
-import React from 'react';
-import { ThemeProvider } from 'styled-components';
-import { GlobalStyles } from '../theme/GlobalStyles';
-import * as theme from '../theme';
+import { useServerInsertedHTML } from 'next/navigation';
+import React, { useState } from 'react';
+import { ServerStyleSheet, StyleSheetManager } from 'styled-components';
 
 export default function StyledComponentsRegistry({ children }: { children: React.ReactNode }) {
-  return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      {children}
-    </ThemeProvider>
-  );
+  // Only create stylesheet once with lazy initial state
+  // x-ref: https://reactjs.org/docs/hooks-reference.html#lazy-initial-state
+  const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet());
+
+  useServerInsertedHTML(() => {
+    const styles = styledComponentsStyleSheet.getStyleElement();
+    // @ts-ignore
+    styledComponentsStyleSheet.instance.clearTag();
+    return <>{styles}</>;
+  });
+
+  if (typeof window !== 'undefined') return <>{children}</>;
+
+  return <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>{children}</StyleSheetManager>;
 }
